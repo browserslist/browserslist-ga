afa62855f768bfbf080b493b8ffeb3c50d2ff760
David Francisco <dmfrancisc@gmail.com>
2018-03-11 18:46:29 +0000

    Use `inquirer` to ask questions

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const inquirer = require("inquirer");
 const googleAuth = require("./src/google-auth");
 const googleAnalytics = require("./src/google-analytics");
 
 googleAuth(oauth2Client => {
   googleAnalytics.getAccounts(oauth2Client, accounts => {
-    console.log(accounts);
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "accountId",
+          message: "Please select your account:",
+          choices: accounts.map(account => ({
+            value: account.id,
+            name: `${account.name} (#${account.id})`,
+          })),
+        },
+      ])
+      .then(answers => {
+        console.log(answers.accountId);
+      });
   });
 });
