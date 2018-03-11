7ba7c5ef0ebfa5f90327df5c945d12c3ddd53387
David Francisco <dmfrancisc@gmail.com>
2018-03-11 17:51:49 +0000

    Get access and refresh tokens example

--- index.js
+++ index.js
@@ -16,4 +16,14 @@ console.log("Please open this URL in your browser:", url);
 
 listen(redirectUrl, port, code => {
   console.log("Authorization code is:", code);
+
+  oauth2Client.getToken(code, (err, tokens) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log("Access token is:", tokens.access_token);
+    console.log("Refresh token is:", tokens.refresh_token);
+    oauth2Client.setCredentials(tokens);
+  });
 });
